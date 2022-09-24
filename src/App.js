d953c5cc8868499f0a37641b860127387a9577fc
jmjusmin <jmjusmin1412@gmail.com>
2022-09-24 14:52:54 -0700

    Learning about conditional in JSX

--- src/App.js
+++ src/App.js
@@ -6,21 +6,32 @@ function App(){
         {id:1, text:'Comment one'},
         {id:2, text:'Comment two'},
         {id:3, text:'Comment three'},
-    ]
+    ];
+
+    const loading = false;
+    const showComments = true;
+
+    if(loading) return <h1>Loading...</h1>
+
+    const commentBlock = (
+    <div className="comment">
+        <h3>Comment({comments.length})</h3>
+        <ul>
+            {comments.map((comment,index)=>(
+                //can also use comment.id also
+                <li key={index}>{comment.text}</li>
+            ))}
+        </ul>
+    </div>);
+
     return (
         //we can either use <div> of <>(fragment) to wrap the element
         <div className='container'>
             <h1>{title}</h1>
             <p>{body}</p>
-            <div className="comment">
-                <h3>Comment({comments.length})</h3>
-                <ul>
-                    {comments.map((comment,index)=>(
-                        //can also use comment.id also
-                        <li key={index}>{comment.text}</li>
-                    ))}
-                </ul>
-            </div>
+
+            {showComments && commentBlock}
+            
         </div>
     )
 }
