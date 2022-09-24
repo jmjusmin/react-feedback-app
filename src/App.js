3742cb69ce75d311138538db75493ba3049f00f9
jmjusmin <jmjusmin1412@gmail.com>
2022-09-24 15:42:55 -0700

    Add style to app

--- src/App.js
+++ src/App.js
@@ -1,38 +1,15 @@
+import Header from "./components/Header";
+
 //JSX- allow to return html element | need only one parent element to return
 function App(){
-    const title = 'Blog Post';
-    const body = 'This is my blog post';
-    const comments = [
-        {id:1, text:'Comment one'},
-        {id:2, text:'Comment two'},
-        {id:3, text:'Comment three'},
-    ];
-
-    const loading = false;
-    const showComments = true;
-
-    if(loading) return <h1>Loading...</h1>
-
-    const commentBlock = (
-    <div className="comment">
-        <h3>Comment({comments.length})</h3>
-        <ul>
-            {comments.map((comment,index)=>(
-                //can also use comment.id also
-                <li key={index}>{comment.text}</li>
-            ))}
-        </ul>
-    </div>);
-
     return (
-        //we can either use <div> of <>(fragment) to wrap the element
-        <div className='container'>
-            <h1>{title}</h1>
-            <p>{body}</p>
-
-            {showComments && commentBlock}
-            
-        </div>
+        <>
+            <Header />
+            {/* we can either use <div> of <>(fragment) to wrap the element */}
+            <div className='container'>
+                <h1>My App</h1>
+            </div> 
+        </>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDom.render(
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
     <React.StrictMode>
         <App/>
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+    </React.StrictMode>);
