a2218a2661e3429fe8eb75d352b049e0cb833efb
jmjusmin <jmjusmin1412@gmail.com>
2022-09-25 22:44:41 -0700

    Managing global state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
 
 //JSX- allow to return html element | need only one parent element to return
 function App(){
+    //app level state
+    const [feedback, setFeedback] = useState(FeedbackData);
+    
     return (
         <>
             <Header />
             {/* we can either use <div> of <>(fragment) to wrap the element */}
             <div className='container'>
-                <h1>My App</h1>
+                <FeedbackList feedback={feedback}/>
             </div> 
         </>
     )
---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import FeedbackItem from './FeedbackItem'
+
+function FeedbackList({feedback}) {
+    if(!feedback || feedback.lenght === 0){
+        return <p>No feed back yet.</p>
+    }
+  return (
+    <div className='feedback-list'>
+        {feedback.map((item)=>(
+            <FeedbackItem key={item.id} item={item}/>
+        ))}
+    </div>
+  )
+}
+
+export default FeedbackList
