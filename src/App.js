e87a9347706acd588e73eb0cee871d3c95f94225
jmjusmin <jmjusmin1412@gmail.com>
2022-09-27 13:53:49 -0700

    Create a global button class

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
 import FeedbackData from "./data/FeedbackData";
 
 
@@ -21,6 +22,7 @@ function App(){
             <Header />
             {/* we can either use <div> of <>(fragment) to wrap the element */}
             <div className='container'>
+                <FeedbackForm/>
                 <FeedbackStats feedback={feedback}/>
                 <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
             </div> 
